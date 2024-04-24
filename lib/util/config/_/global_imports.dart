9e9bebd2f67d63e9aba469d0d51e5cd0b92e86b6
june lee <melodysdreamj@gmail.com>
2024-04-24 19:39:48 +0900

    `

--- lib/util/config/_/global_imports.dart
+++ lib/util/config/_/global_imports.dart
@@ -1,5 +1,3 @@
-export '../../_/shared_params/hive_module/_.dart'; //@add
-
 export '../../_/shared_params/_/start_app_params.dart';
 
 export 'package:flutter/services.dart';
@@ -7,4 +5,3 @@ export 'package:flutter/services.dart';
 export '../../_/initial_app/build_app_widget/build_run_app/_.dart';
 
 export 'package:june_flow_util/june_flow_util.dart';
-
